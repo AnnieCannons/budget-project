1c24f36a49e1c5cda6f1ef2e9c1e5cce5d379873
Dorcus Nagadya <dorcusnagadya889@gmail.com>
2024-01-02 12:30:25 -0800

    update

--- project/scripts.js
+++ project/scripts.js
@@ -1,36 +1,35 @@
-// Questions
+ let person = "";
+ let budget = 0;
+ let needs = 0;
+ let savings = 0;
+ let wants = 0;
+ 
 
-// 1. How many child nodes does the body element have in this document?
+ 
+ let beginButton = document.getElementById("begin-button");
+ beginButton.addEventListener("click", myBudget);
 
-// 2. What is the relationship between the div with the class of todo-section and the list element with the text of "Reading"?
+ let welcome = document.getElementById("welcome-message");
+ welcomeButton.addEventListener("click", welcome)
 
-// 3. What is the relationship between the div with the class of image-gallery and the html element?
+function myBudget() {
+    person = prompt ("What is your name");
+    budget = prompt ("Whats your monthly salary?");
+    needs = budget*.5;
+    savings = budget*.2;
+    wants = budget*.3;
+    welcome.textContent = "Hello " + person + "! Here is your budget for the month: " + budget; 
+    console.log("click");
+    console.log(person);
+};
+
+let needsButton = document.getElementById("needs-button");
+needsButton.addEventListener("click", inputYourNeeds);
+
+function inputYourNeeds() {
+    person = prompt ("how much do you spend each month on housing")
+}
 
 
-// 4. Uncomment the following code:
-document.addEventListener('DOMContentLoaded', function () {
-    console.log('skgjnskjgn')
-    const addButton = document.getElementById('addButton');
-    const newTaskInput = document.getElementById('newTask');
-    const taskList = document.getElementById('taskList');
-
-    addButton.addEventListener('click', function () {
-        const taskText = newTaskInput.value;
-
-        if (taskText !== '') {
-            const taskItem = document.createElement('li');
-            taskItem.textContent = taskText;
-
-            taskItem.addEventListener('click', function () {
-                taskItem.classList.toggle('completed');
-            });
-
-            taskList.appendChild(taskItem);
-            newTaskInput.value = '';
-        }
-    });
-});
-
-// Connect this file to the HTML file and open the HTML file in the browser? What do you see? What can you learn from the above code?
-
+  
 
